3754da486583e0066a760226faaa45bee939d762
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-24 02:26:30 +0530

    tets

--- eks/helm.tf
+++ eks/helm.tf
@@ -11,7 +11,9 @@ locals {
 
 provider "helm" {
   kubernetes = {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.demo.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.demo.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.demo.token
   }
 }
 
