277aa43a2ec3288bce636b3ca2d2633ab0f4d0b8
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-23 23:37:12 +0530

    test

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,11 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "amey-test-terraform"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    bucket  = "amey-test-terraform"
+    region  = "us-east-1"
+    key     = "eks/terraform.tfstate"
+    encrypt = true
   }
 }
 provider "aws" {
