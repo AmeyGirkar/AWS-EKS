cbb6ee92ae9a5ab44293733a4729b11eef5453a0
AmeyGirkar <amey.girkar.xie@gmail.com>
2025-11-24 02:06:49 +0530

    test

---
+++ eks/helm.tf
@@ -0,0 +1,50 @@
+# Install Helm-managed releases (ingress-nginx and Argo CD) using the Terraform Helm provider
+# This file expects `data.aws_eks_cluster.demo` and `data.aws_eks_cluster_auth.demo` to be present
+# and `aws_eks_cluster.demo_eks` to exist in the same module (they are defined in `eks.tf`).
+
+locals {
+  ingress_namespace = "ingress-nginx"
+  argocd_namespace  = "argocd"
+  ingress_release   = "ingress-nginx"
+  argocd_release    = "argocd"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+# NGINX Ingress Controller (Helm chart)
+resource "helm_release" "ingress_nginx" {
+  name       = local.ingress_release
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = local.ingress_namespace
+
+  create_namespace = true
+  wait             = true
+  timeout          = 600
+
+  # Example: override values if needed
+  # values = [file("./helm/ingress-values.yaml")]
+
+  depends_on = [aws_eks_cluster.demo_eks]
+}
+
+# Argo CD (Helm chart from argo-helm)
+resource "helm_release" "argocd" {
+  name       = local.argocd_release
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = local.argocd_namespace
+
+  create_namespace = true
+  wait             = true
+  timeout          = 600
+
+  # Example: override values for argocd via a values file
+  # values = [file("./helm/argocd-values.yaml")]
+
+  depends_on = [aws_eks_cluster.demo_eks]
+}
